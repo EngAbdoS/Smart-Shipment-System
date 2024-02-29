7e5e4253a7cb903209e87e8470d234b71d57e3c3
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-02-29 03:32:52 +0200

    update color manager

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -4,12 +4,14 @@ class ColorManager
 {
   static Color primary=const Color(0xffFDB706);//ff m=>100%
   static Color black=const Color(0xff000000);
-  static Color darkGray=const Color(0xffA4A3A3);
+  static Color darkGray=const Color(0xff8D8484);
+  static Color gray=const Color(0xffA4A3A3);
   static Color lightGray=const Color(0xffD9D9D9);
   static Color white=const Color(0xffFFFFFF);
   static Color offWhite=const Color(0xffEEEDEB);
-
-  static Color error=const Color(0xffe61f34);
+  static Color darkGreen =const Color(0xff78B159);
+  static Color lightGreen =const Color(0xff2AD352);
+  static Color error=const Color(0xffDB3A3C);
 
 
 
