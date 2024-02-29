9d7f0b6ba55f76656f7ac1d73b44e77ca50dd589
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-02-29 03:25:31 +0200

    color manager

---
+++ lib/presentation/resources/color_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorManager
+{
+  static Color primary=const Color(0xffFDB706);//ff m=>100%
+  static Color black=const Color(0xff000000);
+  static Color darkGray=const Color(0xffA4A3A3);
+  static Color lightGray=const Color(0xffD9D9D9);
+  static Color white=const Color(0xffFFFFFF);
+  static Color offWhite=const Color(0xffEEEDEB);
+
+  static Color error=const Color(0xffe61f34);
+
+
+
+}
