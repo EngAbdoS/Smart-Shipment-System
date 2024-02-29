8dba2b116c80cea506edd72e18269e6565404d86
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-02-29 05:56:52 +0200

    add assets_manager

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,15 @@
+const String IMAGE_PATH = "assets/images";
+const String JSON_PATH = "assets/json";
+
+class ImageAssets {
+ // static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
+  static const String empty = "$IMAGE_PATH/empty.png";
+
+}
+
+class JsonAssets {
+
+  static const String empty = "$JSON_PATH/empty.json";
+
+
+}
