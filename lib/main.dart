ec69af4b74e0d4c3893934f4df3a95e309673c15
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-04-30 21:06:15 +0300

    add firebase && fire storage && google maps dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:smart_shipment_system/app/App.dart';
+import 'package:smart_shipment_system/firebase_options.dart';
 import 'package:smart_shipment_system/presentation/resources/language_manager.dart';
 import 'app/dependancy_injection.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initAppModule();
   runApp(
     EasyLocalization(
