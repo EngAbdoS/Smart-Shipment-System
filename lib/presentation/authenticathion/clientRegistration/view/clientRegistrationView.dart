766e8b7a73adcaec5bb89211fedb2943bf14c0d6
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-05-19 02:43:15 +0300

    refactor clientRegistrationView

--- lib/presentation/authenticathion/clientRegistration/view/clientRegistrationView.dart
+++ lib/presentation/authenticathion/clientRegistration/view/clientRegistrationView.dart
@@ -5,7 +5,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:smart_shipment_system/app/dependancy_injection.dart';
-
 import 'package:smart_shipment_system/presentation/authenticathion/clientRegistration/viewModel/clientRegistrationViewModel.dart';
 import 'package:smart_shipment_system/presentation/authenticathion/widgets/authWidgets.dart';
 import 'package:smart_shipment_system/presentation/resources/color_manager.dart';
@@ -20,22 +19,12 @@ class ClientRegistrationView extends StatelessWidget {
 
   final ClientRegistrationViewModel _viewModel =
       instance<ClientRegistrationViewModel>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
-  final TextEditingController _phoneNumberController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _birthDateController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: mainClientRegistrationWidget(context));
-
   }
 
   SingleChildScrollView mainClientRegistrationWidget(BuildContext context) {
@@ -50,44 +39,28 @@ class ClientRegistrationView extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               authLogoWidget(),
-
               Text(
                 AppStrings.createAcc,
                 style: Theme.of(context).textTheme.titleMedium,
               ).tr(),
-
-              nameInputWidget(_viewModel.outputIsFirstNameValid,
-                  _viewModel.setFirstName),
               SizedBox(
                 height: 15.sp,
               ),
-            //  nameWidget(),
-            //   const SizedBox(
-            //     height: AppSize.s28,
-            //   ),
+              nameInputWidget(
+                  _viewModel.outputIsFirstNameValid, _viewModel.setFirstName),
+              SizedBox(
+                height: 15.sp,
+              ),
               phoneNumberInputWidget(_viewModel.outputIsPhoneNumberValid,
                   _viewModel.setPhoneNumber),
               SizedBox(
                 height: 15.sp,
               ),
-              // phoneNumberWidget(),
-              // const SizedBox(
-              //   height: AppSize.s28,
-              // ),
-              // addressWidget(),
-              // const SizedBox(
-              //   height: AppSize.s28,
-              // ),
-              emailInputWidget(_viewModel.outputIsEmailValid,
-                  _viewModel.setEmail),
+              emailInputWidget(
+                  _viewModel.outputIsEmailValid, _viewModel.setEmail),
               SizedBox(
                 height: 15.sp,
               ),
-
-              // emailWidget(),
-              // const SizedBox(
-              //   height: AppSize.s28,
-              // ),
               passwordWidgets(
                   _viewModel.outputIsPasswordValid,
                   _viewModel.outputIsConfirmPasswordValid,
@@ -98,17 +71,8 @@ class ClientRegistrationView extends StatelessWidget {
                   _viewModel.validateConfirmPassword,
                   _viewModel.changePasswordState,
                   _viewModel.changeConfirmPasswordState),
-              // passwordWidgets(),
-              // const SizedBox(
-              //   height: AppSize.s28,
-              // ),
-              dateOfBirthWidget(context),
-              const SizedBox(
-                height: AppSize.s28,
-              ),
-              genderWidget(context),
-              const SizedBox(
-                height: AppSize.s28,
+              SizedBox(
+                height: 30.sp,
               ),
               RegularButton(
                 buttonAction: () => _viewModel.getLoading(context),
@@ -125,143 +89,6 @@ class ClientRegistrationView extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Widget addressWidget() {
-    return StreamBuilder<bool>(
-        stream: _viewModel.outputIsAddressValid,
-        builder: (context, snapshot) {
-          return TextFormField(
-            onChanged: (address) => _viewModel.setAddress(address),
-            keyboardType: TextInputType.streetAddress,
-            controller: _addressController,
-            decoration: InputDecoration(
-              // label: Text(AppStrings.username.tr()),
-              hintText: AppStrings.addressHint.tr(),
-              labelText: AppStrings.address.tr(),
-              errorText:
-                  (snapshot.data ?? true) ? null : AppStrings.addressHint.tr(),
-            ),
-          );
-        });
-  }
-
-  Widget emailWidget() {
-    return StreamBuilder<bool>(
-        stream: _viewModel.outputIsEmailValid,
-        builder: (context, snapshot) {
-          return TextFormField(
-            onChanged: (email) => _viewModel.setEmail(email),
-            keyboardType: TextInputType.emailAddress,
-            controller: _emailController,
-            decoration: InputDecoration(
-              // label: Text(AppStrings.username.tr()),
-              hintText: AppStrings.emailHint.tr(),
-              labelText: AppStrings.emailHint.tr(),
-              errorText:
-                  (snapshot.data ?? true) ? null : AppStrings.invalidEmail.tr(),
-            ),
-          );
-        });
-  }
-
-  Widget phoneNumberWidget() {
-    return StreamBuilder<bool>(
-        stream: _viewModel.outputIsPhoneNumberValid,
-        builder: (context, snapshot) {
-          return TextFormField(
-            onChanged: (phoneNumber) => _viewModel.setPhoneNumber(phoneNumber),
-            keyboardType: TextInputType.phone,
-            controller: _phoneNumberController,
-            decoration: InputDecoration(
-              hintText: AppStrings.phoneNumberHint.tr(),
-              labelText: AppStrings.phoneNumber.tr(),
-              errorText: (snapshot.data ?? true)
-                  ? null
-                  : AppStrings.phoneNumberError.tr(),
-            ),
-          );
-        });
-  }
-
-  Widget passwordsWidget() {
-    return Column(
-      children: [
-        StreamBuilder<bool>(
-          builder: (context, snapshot) {
-            return StreamBuilder<bool>(
-                stream: _viewModel.outputIsPasswordHidden,
-                builder: (context, hiddenState) {
-                  return TextFormField(
-                    onChanged: (password) => {
-                      _viewModel.setPassword(password),
-                      _viewModel.validateConfirmPassword(),
-                    },
-                    obscureText: hiddenState.data ?? true,
-                    keyboardType: TextInputType.visiblePassword,
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            _viewModel.changePasswordState();
-                          },
-                          icon: Icon(
-                            (hiddenState.data ?? true)
-                                ? Icons.remove_red_eye_rounded
-                                : Icons.remove_red_eye_outlined,
-                            color: ColorManager.primary,
-                          ),
-                        ),
-                        hintText: AppStrings.password.tr(),
-                        labelText: AppStrings.password.tr(),
-                        errorText: (snapshot.data ?? true)
-                            ? null
-                            : AppStrings.passwordInvalid.tr(),
-                        errorMaxLines: 2),
-                  );
-                });
-          },
-          stream: _viewModel.outputIsPasswordValid,
-        ),
-        const SizedBox(
-          height: AppSize.s28,
-        ),
-        StreamBuilder<bool>(
-          builder: (context, snapshot) {
-            return StreamBuilder<bool>(
-                stream: _viewModel.outputIsConfirmPasswordHidden,
-                builder: (context, hiddenState) {
-                  return TextFormField(
-                    onChanged: (confirmPassword) =>
-                        _viewModel.setConfirmPassword(confirmPassword),
-                    obscureText: hiddenState.data ?? true,
-                    keyboardType: TextInputType.visiblePassword,
-                    controller: _confirmPasswordController,
-                    decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            _viewModel.changeConfirmPasswordState();
-                          },
-                          icon: Icon(
-                            (hiddenState.data ?? true)
-                                ? Icons.remove_red_eye_rounded
-                                : Icons.remove_red_eye_outlined,
-                            color: ColorManager.primary,
-                          ),
-                        ),
-                        hintText: AppStrings.confirmPass.tr(),
-                        labelText: AppStrings.confirmPass.tr(),
-                        errorText: (snapshot.data ?? true)
-                            ? null
-                            : AppStrings.confirmPassError.tr(),
-                        errorMaxLines: 2),
-                  );
-                });
-          },
-          stream: _viewModel.outputIsConfirmPasswordValid,
-        ),
-      ],
     );
   }
 
@@ -292,144 +119,5 @@ class ClientRegistrationView extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Widget nameWidget() {
-    return Container(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Expanded(
-            child: StreamBuilder<bool>(
-                stream: _viewModel.outputIsFirstNameValid,
-                builder: (context, snapshot) {
-                  return TextFormField(
-                    onChanged: (firstName) =>
-                        _viewModel.setFirstName(firstName),
-                    keyboardType: TextInputType.name,
-                    controller: _firstNameController,
-                    decoration: InputDecoration(
-                      // label: Text(AppStrings.username.tr()),
-                      hintText: AppStrings.fNameHint.tr(),
-                      labelText: AppStrings.fName.tr(),
-                      errorText: (snapshot.data ?? true)
-                          ? null
-                          : AppStrings.fNameHint.tr(),
-                    ),
-                  );
-                }),
-          ),
-          const SizedBox(
-            width: AppSize.s8 * 2,
-          ),
-          Expanded(
-            child: StreamBuilder<bool>(
-                stream: _viewModel.outputIsLastNameValid,
-                builder: (context, snapshot) {
-                  return TextFormField(
-                    onChanged: (lastName) => _viewModel.setLastName(lastName),
-                    keyboardType: TextInputType.name,
-                    controller: _lastNameController,
-                    decoration: InputDecoration(
-                      // label: Text(AppStrings.username.tr()),
-                      hintText: AppStrings.lNameHint.tr(),
-                      labelText: AppStrings.lName.tr(),
-                      errorText: (snapshot.data ?? true)
-                          ? null
-                          : AppStrings.lNameHint.tr(),
-                    ),
-                  );
-                }),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget dateOfBirthWidget(BuildContext context) {
-    DateTime? pickedDate;
-    return StreamBuilder<bool>(
-        stream: _viewModel.outputIsBirthDayValid,
-        builder: (context, snapshot) {
-          return TextFormField(
-            //  enabled: false,
-            readOnly: true,
-            onTap: () async {
-              pickedDate = await showDatePicker(
-                  initialEntryMode: DatePickerEntryMode.calendarOnly,
-                  // initialEntryMode=DatePickerEntryMode.input,
-                  // barrierColor: ColorManager.primary,
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1950),
-                  lastDate: DateTime(2100));
-              _viewModel.setBirthDate(pickedDate ?? DateTime(0));
-              _birthDateController.text =
-                  "${pickedDate?.year ?? "0"}-${pickedDate?.month ?? "0"}-${pickedDate?.day ?? "0"}";
-            },
-
-            controller: _birthDateController,
-            decoration: InputDecoration(
-              hintText: AppStrings.dateBirthError.tr(),
-              labelText: AppStrings.dateBirth.tr(),
-              errorText: (snapshot.data ?? true)
-                  ? null
-                  : AppStrings.dateBirthError.tr(),
-            ),
-          );
-        });
-  }
-
-  Widget genderWidget(BuildContext context) {
-    return StreamBuilder<bool>(
-        stream: _viewModel.outputIsGenderManValid,
-        builder: (context, snapshot) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () => _viewModel.setGender(true),
-                child: Container(
-                  height: AppSize.s24,
-                  width: AppSize.s24,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (snapshot.data ?? false)
-                          ? ColorManager.primary
-                          : ColorManager.gray),
-                ),
-              ),
-              const SizedBox(
-                width: AppSize.s8,
-              ),
-              Text(
-                AppStrings.male,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ).tr(),
-              const SizedBox(
-                width: AppSize.s18 * 2,
-              ),
-              GestureDetector(
-                onTap: () => _viewModel.setGender(false),
-                child: Container(
-                  height: AppSize.s24,
-                  width: AppSize.s24,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (snapshot.data ?? true)
-                          ? ColorManager.gray
-                          : ColorManager.primary),
-                ),
-              ),
-              const SizedBox(
-                width: AppSize.s8,
-              ),
-              Text(
-                AppStrings.female,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ).tr(),
-            ],
-          );
-        });
   }
 }
