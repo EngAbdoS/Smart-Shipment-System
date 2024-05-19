981a74c89361f74803c68b6280f0f39040bd3be9
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-05-20 01:34:57 +0300

    create client rg request

--- lib/data/network/requests.dart
+++ lib/data/network/requests.dart
@@ -5,4 +5,14 @@ class LoginRequest {
   LoginRequest(this.email, this.password);
 }
 
+class ClientRegistrationRequest {
+  String name;
+  String email;
+  String phone;
+  String password;
+  String confirmPassword;
+  String role;
 
+  ClientRegistrationRequest(this.name, this.email, this.phone, this.password,
+      this.confirmPassword, this.role);
+}
