9242c796dd9e768dc1be87cdc9364d088ea50757
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-05-20 01:48:33 +0300

    create client rg endPoint

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -3,15 +3,23 @@ import 'package:retrofit/http.dart';
 import 'package:smart_shipment_system/app/app_constants.dart';
 import 'package:smart_shipment_system/data/response/response.dart';
 
-
 part 'app_api.g.dart';
 
 @RestApi(baseUrl: AppConstants.baseUrl) //annotation
 abstract class AppServiceClient {
-  factory AppServiceClient(Dio dio , {String baseUrl}) = _AppServiceClient;
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
 
   @POST("users/login")
   Future<AuthenticationResponse> login(
       @Field("email") String email, @Field("password") String password);
 
+  @POST("users/signup")
+  Future<AuthenticationResponse> clientRegistration(
+    @Field("name") String name,
+    @Field("email") String email,
+    @Field("phone") String phone,
+    @Field("password") String password,
+    @Field("passwordConfirm") String passwordConfirm,
+    @Field("role") String role,
+  );
 }
--- lib/data/network/app_api.g.dart
+++ lib/data/network/app_api.g.dart
@@ -53,6 +53,47 @@ class _AppServiceClient implements AppServiceClient {
     return value;
   }
 
+  @override
+  Future<AuthenticationResponse> clientRegistration(
+    String name,
+    String email,
+    String phone,
+    String password,
+    String passwordConfirm,
+    String role,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = {
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'password': password,
+      'passwordConfirm': passwordConfirm,
+      'role': role,
+    };
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AuthenticationResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'users/signup',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = AuthenticationResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
