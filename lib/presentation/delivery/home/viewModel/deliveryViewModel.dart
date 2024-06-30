d0ff518b30cd81f14fa2212343d821c3c9af9006
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-06-30 16:11:15 +0300

    deliveryGetOrders pagination fun

--- lib/presentation/delivery/home/viewModel/deliveryViewModel.dart
+++ lib/presentation/delivery/home/viewModel/deliveryViewModel.dart
@@ -1,18 +1,47 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import 'package:smart_shipment_system/domain/models/shipmentModel.dart';
 import 'package:smart_shipment_system/domain/models/userModel.dart';
 import 'package:smart_shipment_system/domain/repository/repository.dart';
 import 'package:smart_shipment_system/presentation/delivery/main/viewModel/mainDeliveryViewModel.dart';
+import 'package:smart_shipment_system/presentation/widgets/errorState.dart';
+import 'package:smart_shipment_system/presentation/widgets/hideState.dart';
+import 'package:smart_shipment_system/presentation/widgets/loadingState.dart';
 
 class DeliveryHomeViewModel extends MainDeliveryViewModel {
-  DeliveryHomeViewModel(this._repository, this.userHomeData) : super(_repository);
+  DeliveryHomeViewModel(this._repository, this.userHomeData)
+      : super(_repository);
 
   UserModel userHomeData;
+  int fetchPageIndex = 1;
+  List<ShipmentModel> ordersList = [];
   final Repository _repository;
+  final StreamController _ordersListStreamController =
+      BehaviorSubject<List<ShipmentModel>?>();
+
+  Stream<List<ShipmentModel>?> get outputOrdersList =>
+      _ordersListStreamController.stream.map((ordersList) => ordersList);
+
+  Sink get inputOrdersList => _ordersListStreamController.sink;
+
   startHomeView(dynamic context) async {
-
-
-
-
+    fetchPageIndex = 1;
+    await getAllShipments(context);
   }
+
+  Future getAllShipments(dynamic context) async {
+    loadingState(context: context);
+    (await _repository.deliveryGetOrders(fetchPageIndex)).fold(
+        (failure) => {
+              errorState(context: context, message: failure.message),
+            }, (data) async {
+      ordersList.addAll(data);
+      data.isNotEmpty ? fetchPageIndex++ : null;
+      inputOrdersList.add(ordersList);
+      hideState(context: context);
+    });
+  }
+
 
 
 
