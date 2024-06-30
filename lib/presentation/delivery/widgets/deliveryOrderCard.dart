13e0e94b6a35c0248a226a6c66a817bbc659e54b
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-06-30 20:10:53 +0300

    create delivery order card

---
+++ lib/presentation/delivery/widgets/deliveryOrderCard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:smart_shipment_system/domain/models/shipmentModel.dart';
+
+Widget deliveryOrderCard(ShipmentModel shipment)
+{
+
+
+
+  return Container();
+
+
+
+}
--- lib/presentation/delivery/widgets/deliveryOrdersList.dart
+++ lib/presentation/delivery/widgets/deliveryOrdersList.dart
@@ -2,40 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_shipment_system/domain/models/shipmentModel.dart';
+import 'package:smart_shipment_system/presentation/delivery/widgets/deliveryOrderCard.dart';
 import 'package:smart_shipment_system/presentation/resources/strings_manager.dart';
 import 'package:smart_shipment_system/presentation/widgets/emptyListWidget.dart';
 
 Widget deliveryOrdersList({
- required BuildContext context,
- required Stream<List<ShipmentModel>?> outputDeliveryOrdersList,
- required Function assignOrderToMe,
- required ScrollController scrollController,
+  required BuildContext context,
+  required Stream<List<ShipmentModel>?> outputDeliveryOrdersList,
+  required Function assignOrderToMe,
+  required ScrollController scrollController,
 }) {
   return StreamBuilder<List<ShipmentModel>?>(
-      stream: outputDeliveryOrdersList,
-      builder: (context, snapshot) {
-
-        return Container(
-          // height: 200,
-          padding: EdgeInsets.only(bottom: 25.sp),
-          child: (snapshot.hasData && snapshot.data!.isNotEmpty)
-              ? ListView.builder(
-            controller: scrollController,
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemBuilder: (context, index) => Container(
-              child: Text(snapshot.data![index].date??""),
-            )
-                .animate()
-                .slideY(
-                duration: 300.milliseconds,
-                curve: Curves.bounceInOut),
-            itemCount: snapshot.data?.length ?? 0,
-          ).animate().shake(curve: Curves.bounceInOut)
-              : emptyListWidget(
-            context,
-            message: AppStrings.on_data,
-          ),
-        );
-      });
+    stream: outputDeliveryOrdersList,
+    builder: (context, snapshot) {
+      return Container(
+        // height: 200,
+        padding: EdgeInsets.only(bottom: 25.sp),
+        child: (snapshot.hasData && snapshot.data!.isNotEmpty)
+            ? ListView.builder(
+                controller: scrollController,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context, index) =>
+                    deliveryOrderCard(snapshot.data![index]).animate().slideY(
+                        duration: 300.milliseconds, curve: Curves.bounceInOut),
+                itemCount: snapshot.data?.length ?? 0,
+              ).animate().shake(curve: Curves.bounceInOut)
+            : emptyListWidget(
+                context,
+                message: AppStrings.on_data,
+              ),
+      );
+    },
+  );
 }
